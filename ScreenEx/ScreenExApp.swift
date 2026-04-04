2c9109956d208f40935b662c707a6c5dab0f465d
Nastya <mail@yuhimenko-anastasia.ru>
2026-04-04 12:38:53 +0300

    Initial Commit

---
+++ ScreenEx/ScreenExApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenExApp.swift
+//  ScreenEx
+//
+//  Created by Anastasia on 04.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct ScreenExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
